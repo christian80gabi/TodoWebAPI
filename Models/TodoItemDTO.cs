f534dac20bffa5f68638920dfee751da810af35d
Christian Gabi <49684157+christian80gabi@users.noreply.github.com>
2023-04-02 19:41:08 +0100

    Pushing project first commit

---
+++ Models/TodoItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoWebAPI.Models
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
